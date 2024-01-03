3f10d8d50ee1fc23dce71436a357b282b23bd813
loids <lorraynemfg>
2024-01-03 00:54:03 -0300

    rota exercicios com condicional de combinação inválida

--- GYM-API/src/controladores/exercicio_controller.js
+++ GYM-API/src/controladores/exercicio_controller.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const {q_exercicio_body} = require('../config/api_exercicios');
-const muscle = require("../services/service")
 const exercicios = async (req, res) => {
     
     try {
@@ -13,7 +12,9 @@ const exercicios = async (req, res) => {
         const filtro=responsePartesDoCorpo.data.filter((i) => {
             return i.target===req.body.alvo || i.secondaryMuscles.some((muscle)=> muscle=== req.body.alvo)
         })
-        
+        if (filtro.length < 1) {
+           return res.json({mensagem: "Nenhum exercício encontrado com essa combinação. ):"})
+        }
         return res.json(filtro)
     } catch (error) {
         return res.status(500).json({ error: 'Erro interno do servidor.' });
